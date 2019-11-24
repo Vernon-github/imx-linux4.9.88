8d11a2a4e3780c486fe33d25caedd21ebc573922
Vernon-github <2669889282@qq.com>
2019-11-24 14:31:20 +0800

    add: ap3216c:  chrdev driver register
    Change-Id: Id8703bea1aa86aa7233d9d2af1c31002262f9a53

--- drivers/i2c/i2c-slave-ap3216c.c
+++ drivers/i2c/i2c-slave-ap3216c.c
@@ -3,13 +3,60 @@
 #include <linux/module.h>
 #include <linux/i2c.h>
 #include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
 
 struct ap3216c_dev {
 	unsigned char addr;				/* chip address - NOTE: 7bit*/
 	struct i2c_adapter *adapter;	/* the adapter we sit on	*/
 	struct device dev;				/* the device structure		*/
+
+	dev_t devid;
+	struct cdev cdev;
+	struct class *class;
 };
 struct ap3216c_dev ap3216cDev;
+
+
+int ap3216c_open(struct inode *inode, struct file *file)
+{
+	struct device dev = ap3216cDev.dev;
+
+	dev_dbg(&dev, "%s\n", __func__);
+
+	return 0;
+}
+
+int ap3216c_release(struct inode *inode, struct file *file)
+{
+	struct device dev = ap3216cDev.dev;
+
+	dev_dbg(&dev, "%s\n", __func__);
+
+	return 0;
+}
+
+ssize_t ap3216c_read(struct file *file, char __user *buf, size_t size, loff_t *offset)
+{
+	struct device dev = ap3216cDev.dev;
+	unsigned char val;
+	int ret;
+
+	dev_dbg(&dev, "%s\n", __func__);
+
+	val = 1;
+	ret = copy_to_user(buf, &val, size);
+
+	return ret;
+}
+
+const struct file_operations ap3216c_fops = {
+	.owner    = THIS_MODULE,
+	.open     = ap3216c_open,
+	.release  = ap3216c_release,
+	.read     = ap3216c_read,
+};
 
 int ap3216c_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
@@ -21,6 +68,12 @@ int ap3216c_probe(struct i2c_client *client, const struct i2c_device_id *id)
 
 	dev_dbg(&dev, "%s: i2c addr 0x%x[7bits]\n", __func__, ap3216cDev.addr);
 
+	alloc_chrdev_region(&ap3216cDev.devid, 0, 1, "ap3216cCharDev");
+	cdev_init(&ap3216cDev.cdev, &ap3216c_fops);
+	cdev_add(&ap3216cDev.cdev, ap3216cDev.devid, 1);
+	ap3216cDev.class = class_create(THIS_MODULE, "ap3216cClass");
+	device_create(ap3216cDev.class, NULL, ap3216cDev.devid, NULL, "ap3216cDev");
+
 	return 0;
 }
 
@@ -29,6 +82,11 @@ int ap3216c_remove(struct i2c_client *client)
 	struct device dev = client->dev;
 
 	dev_dbg(&dev, "%s\n", __func__);
+
+	device_destroy(ap3216cDev.class, ap3216cDev.devid);
+	class_destroy(ap3216cDev.class);
+	cdev_del(&ap3216cDev.cdev);
+	unregister_chrdev_region(ap3216cDev.devid, 1);
 
 	return 0;
 }
