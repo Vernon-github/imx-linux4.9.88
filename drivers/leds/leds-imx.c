03a328057649fed03756ed9e5c8fbe44df93f801
Vernon-github <2669889282@qq.com>
2019-12-26 08:46:52 +0800

    add: led in the integer-based GPIO interface – legacy
    Change-Id: I4b5bd81d69999d0d197b2a587bf6901b0250e47a

---
+++ drivers/leds/leds-imx.c
@@ -0,0 +1,125 @@
+#define DEBUG
+
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/of_gpio.h>
+#include <linux/gpio.h>
+#include <linux/cdev.h>
+#include <asm/uaccess.h>
+
+struct led_dev {
+	struct platform_device *pdev;
+	unsigned int led_gpio;
+#define LED_ON  0
+#define LED_OFF 1
+
+	dev_t devid;
+	struct cdev cdev;
+	struct class *class;
+
+};
+struct led_dev ledDev;
+
+int led_open(struct inode *inode, struct file *file)
+{
+	struct platform_device *pdev = ledDev.pdev;
+	struct device dev = pdev->dev;
+
+	dev_dbg(&dev, "%s: \n", __func__);
+
+	return 0;
+}
+
+int led_release(struct inode *inode, struct file *file)
+{
+	struct platform_device *pdev = ledDev.pdev;
+	struct device dev = pdev->dev;
+
+	dev_dbg(&dev, "%s: \n", __func__);
+
+	return 0;
+}
+
+ssize_t led_write(struct file *file, const char __user *buf, size_t size, loff_t *offset)
+{
+	struct platform_device *pdev = ledDev.pdev;
+	struct device dev = pdev->dev;
+	unsigned char status, ret;
+
+	ret = copy_from_user(&status, buf, size);
+	dev_dbg(&dev, "%s: status %d\n", __func__, status);
+
+	if(status)
+		gpio_set_value(ledDev.led_gpio, LED_ON);
+	else
+		gpio_set_value(ledDev.led_gpio, LED_OFF);
+
+	return 0;
+}
+
+const struct file_operations led_fops = {
+	.owner    = THIS_MODULE,
+	.open     = led_open,
+	.release  = led_release,
+	.write    = led_write,
+};
+
+int led_probe(struct platform_device *pdev)
+{
+	struct device dev = pdev->dev;
+	struct device_node *np = dev.of_node;
+	unsigned int led_gpio;
+
+	dev_dbg(&dev, "%s: \n", __func__);
+
+	led_gpio = of_get_named_gpio(np, "led-gpios", 0);
+	gpio_direction_output(led_gpio, LED_OFF);
+
+	ledDev.pdev = pdev;
+	ledDev.led_gpio = led_gpio;
+
+	alloc_chrdev_region(&ledDev.devid, 0, 1, "ledCharDev");
+	cdev_init(&ledDev.cdev, &led_fops);
+	cdev_add(&ledDev.cdev, ledDev.devid, 1);
+	ledDev.class = class_create(THIS_MODULE, "ledClass");
+	device_create(ledDev.class, NULL, ledDev.devid, NULL, "ledDev");
+
+	return 0;
+}
+
+int led_remove(struct platform_device *pdev)
+{
+	struct device dev = pdev->dev;
+
+	dev_dbg(&dev, "%s: \n", __func__);
+
+	device_destroy(ledDev.class, ledDev.devid);
+	class_destroy(ledDev.class);
+	cdev_del(&ledDev.cdev);
+	unregister_chrdev_region(ledDev.devid, 1);
+
+	return 0;
+}
+
+static const struct of_device_id of_led_match[] = {
+	{ .compatible = "atk,led", },
+	{},
+};
+
+MODULE_DEVICE_TABLE(of, of_led_match);
+
+static struct platform_driver led_driver = {
+	.probe  = led_probe,
+	.remove = led_remove,
+	.driver = {
+		.owner = THIS_MODULE,
+		.name = "led",
+		.of_match_table = of_led_match,
+	},
+};
+
+module_platform_driver(led_driver);
+
+MODULE_AUTHOR("lincheng Yang <2669889282@qq.com>");
+MODULE_DESCRIPTION("led mode");
+MODULE_LICENSE("GPL v2");
